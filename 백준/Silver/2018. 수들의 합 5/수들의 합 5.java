83fe98d1ef6de171f558be118cb10e6d2f6d0667
leedidoo <83906450+leedidoo@users.noreply.github.com>
2024-02-28 10:08:34 +0900

    [Silver V] Title: 수들의 합 5, Time: 164 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count =1; // N이 15일 떄 숫자 15만 뽑는 셩우를 미리 넣고 초기화
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N){
+            if(sum == N){
+                count++;
+                end_index++;
+                sum += end_index;
+            } else if (sum > N) {
+                sum -= start_index;
+                start_index++;
+            }else{
+                end_index++;
+                sum += end_index;
+            }
+        }
+        System.out.println(count);
+    }
+}
