1bb9bff4acee36cfcf2b09f12cb8d494fd221381
leedidoo <83906450+leedidoo@users.noreply.github.com>
2024-03-12 10:27:13 +0900

    [Gold II] Title: 버블 소트, Time: 1680 ms, Memory: 82212 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        mData[] arr = new mData[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new mData(Integer.parseInt(br.readLine()), i);
+        }
+        br.close();
+        Arrays.sort(arr);
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i].index - i) {
+                max = arr[i].index - i;
+            }
+        }
+        System.out.println(max + 1);
+    }
+}
+class mData implements Comparable<mData> {
+    int value;
+    int index;
+
+    public mData(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(mData o) {
+        return this.value - o.value;
+    }
+}
